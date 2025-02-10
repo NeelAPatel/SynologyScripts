a4d971e942d72916d2b29ddd2cf3902dfff5a7a4
Neel Patel <send2neelpatel@gmail.com>
2025-02-10 02:54:58 -0500

    Added function for folder tracking with proper expected formatting and implemented into processor

--- SynoErrorProcessorV2.py
+++ SynoErrorProcessorV2.py
@@ -19,73 +19,127 @@
 # from PIL import ImageFile
 
 
+from collections import defaultdict
 from SynoProcessResults import ProcessingResult
-from string_format_wrap import swrap
-
+from string_format_wrap import swrap 
+import json 
+import os
 # Global Vars
 
 LIST_FILE_TYPE = [".jpg", ".png", ".jpeg", ".mp4", ".gif", ".dng", ".webp", ".mov", ".heic"]
-CURR_FILE_TYPE = ""
 
 LIST_SOURCE_PATH_FOLDERS = [
-    str(r"F:\GPhotos Takeout - Nidhi - Dec31\Takeout\Google Photos - Copy\NEEDSFIXING\ASHP Midyear 23 - Anaheim, CA"), 
-    str(r"F:\GPhotos Takeout - Nidhi - Dec31\Takeout\Google Photos - Copy\NEEDSFIXING\test_folder")
+    r"F:\GPhotos Takeout - Nidhi - Dec31\Takeout\Google Photos - Copy\NEEDSFIXING\White Coat Ceremony _26",
+    r"F:\GPhotos Takeout - Nidhi - Dec31\Takeout\Google Photos - Copy\NEEDSFIXING\Vicky_s Surprise Sweet Sixteen!",
+    r"F:\GPhotos Takeout - Nidhi - Dec31\Takeout\Google Photos - Copy\NEEDSFIXING\Untitled",
+    r"F:\GPhotos Takeout - Nidhi - Dec31\Takeout\Google Photos - Copy\NEEDSFIXING\SUGA _ AGUST D 4-27",
+    r"F:\GPhotos Takeout - Nidhi - Dec31\Takeout\Google Photos - Copy\NEEDSFIXING\SUGA _ AGUST D @ UBS 4-27",
+    # str(r"F:\GPhotos Takeout - Nidhi - Dec31\Takeout\Google Photos - Copy\NEEDSFIXING\ASHP Midyear 23 - Anaheim, CA"), 
+    
+    # str(r"F:\GPhotos Takeout - Nidhi - Dec31\Takeout\Google Photos - Copy\NEEDSFIXING\test_folder")
+    # "E:\Apps"
 ]
-CURR_SOURCE_PATH_FOLDER = ""
-CURR_SOURCE_PATH = None
-
-PROCESSED_PATH = CURR_SOURCE_PATH_FOLDER + "\\Processed"
-SKIPPED_PATH = CURR_SOURCE_PATH_FOLDER + "\\Skipped"
 
 
+results = None
+curr_source_path = None
+curr_source_path_album_folder = ""
+curr_file_type = ""
 
-RESULTS = None
-
-
-
-#<folder> <type> <count/max> <latest complete> <current_working> <completed_list>
-
-
-# Counter = 
-# TotalMediaCounter
-# PerFiletypeCounter
-#
+processed_path = curr_source_path_album_folder + "\\Processed"
+skipped_path = curr_source_path_album_folder + "\\Skipped"
 
 
-def per_folder_traversal(): 
+
+def processing_media(): 
+    # For each folder
+    #for source_path in LIST_SOURCE_PATH_FOLDERS: 
+        
+        #For each file
+        #for root, _, files in os.walk(source_path): 
+            
+            #Create Processed_Filetype if does not exist
+            
+            # Retrieve current date values
+            # retrieve json value
+            # ask to override 
+            # if overwrite --> process action
+                #when processing complete --> move into //Processed_originals
+            # else
+                # let file be and move on
+    return
+
+def scan_all_sources(): 
+    # print(str(results.get_progress_tracker()))
+    
+    print("-------- SCANNING SOURCE PATHS -------------")
     for source_path in LIST_SOURCE_PATH_FOLDERS: 
+        #Loop through all source folders
         
-        CURR_SOURCE_PATH_FOLDER = source_path
+        # For each folder, count total files + count per filetype 
+        total_media_count = 0
+        media_type_aggr = defaultdict(int)
         
-        # traverse that folder for files
+        if not os.path.isdir(source_path):
+            print(swrap("r", f"Skipping non-existent folder: {source_path}"))
+            continue
+            
+        # Traverse all files in the folder to count everything
+        print(f"\nScanning folder: {source_path}")
+        for root, _, files in os.walk(source_path):
+            for file in files:
+                file_ext = os.path.splitext(file)[-1].lower()  # Extract file extension
+                if file_ext in LIST_FILE_TYPE:
+                    media_type_aggr[file_ext] += 1
+                    total_media_count += 1
+        # Print results
+        # print(f"Finished scanning!")
+        print(f"Total count: {total_media_count} -- Type breakdown:", dict(media_type_aggr))
         
-        # traversal is complete
-        result_dict
+        # Add findings to tracker
+        results.add_folder_to_tracking(source_path, total_media_count, media_type_aggr)
+    print("-------- SCANNING FINISHED FOR SOURCE PATHS -------------")
     
     
 def main(): 
     # Pre execution setup
-    print("Synology Photos Metadata Corrector")
+    print(swrap("bold", swrap("blubg", "Synology Photos Metadata Corrector")))
     print("--------------SETUP---------------")
+    global results
+    global curr_source_path
+    global curr_source_path_album_folder
+    global curr_file_type
+    global processed_path
+    global skipped_path
+    
+    
     
     # Print global variables
-    '''
-    - Supported File Types
-    - Processed path
-    - Skipped Path
-    - List of Source Paths
-    '''
     print("Supported Types: ", str(LIST_FILE_TYPE))
     print("Folders to Process: ")
     for folderPath in LIST_SOURCE_PATH_FOLDERS: 
         print(swrap("g", folderPath))
     
     
-    RESULTS = ProcessingResult()
-    RESULTS.set_total_file_types(LIST_FILE_TYPE)
-    RESULTS.set_total_paths(LIST_SOURCE_PATH_FOLDERS)
+    results = ProcessingResult()
+    print(results)
+    results.set_total_paths(LIST_SOURCE_PATH_FOLDERS)
+    results.set_total_file_types(LIST_FILE_TYPE)
+    print("--------------SETUP END ---------------\n")
     
     
+    scan_all_sources()
+    processing_media() #main function
+    # action = input (swrap("b", "Continue to processing menu? [y]"))
+    
+    # if action == "": 
+    #     print("hello" )
+    
+    
+    # if action != "y" or action != "" : 
+    #     exit(0)
+    # else: 
+    #     print("Continuing!")
     # # Wait for user to continue program 
     
     # # Folder(s) Traversal
--- SynoProcessResults.py
+++ SynoProcessResults.py
@@ -1,30 +1,30 @@
-result_dict = {
-    "total_paths_to_process":  [], # Full Paths to process
-    "total_file_types_to_process" : [], # store list of file types being processed at this time
-    "processing_start_time" : "",  # timestamp of when processing starts
-    "processing_end_time": "",
-    "progress_results_file_name" : "",
-    "progres_tracker":  
-        # each folder will have its own object here
-        {
+# result_dict = {
+#     "total_paths_to_process":  [], # Full Paths to process
+#     "total_file_types_to_process" : [], # store list of file types being processed at this time
+#     "processing_start_time" : "",  # timestamp of when processing starts
+#     "processing_end_time": "",
+#     "progress_results_file_name" : "",
+#     "progres_tracker":  
+#         # each folder will have its own object here
+#         {
             
-            "<folder_path>": 
-                {
-                    "folder_name": "", 
-                    "total_detected_media": 0,
-                    "detected_media": {
-                        "<type>": {
-                            "total_count": 0,
-                            "processed_count": 0,
-                            "last_processed_path": None,
-                            "processed_source_paths" :  [],
-                            "processed_destination_paths" :  [],
-                            "errors_source_paths" :  [],                              
-                        }
-                    }
-                }, 
-        },   
-}
+#             "<folder_path>": 
+#                 {
+#                     "folder_name": "", 
+#                     "total_detected_media": 0,
+#                     "detected_media": {
+#                         "<type>": {
+#                             "total_count": 0,
+#                             "processed_count": 0,
+#                             "last_processed_path": None,
+#                             "processed_source_paths" :  [],
+#                             "processed_destination_paths" :  [],
+#                             "errors_source_paths" :  [],                              
+#                         }
+#                     }
+#                 }, 
+#         },   
+# }
 
 
 
@@ -35,6 +35,7 @@ import json
 import os
 
 class ProcessingResult:
+    
     def __init__(self, output_file_name="processing_results.json"):
         
         #General 
@@ -43,10 +44,10 @@ class ProcessingResult:
         self.processing_start_time = None  # Timestamp of when processing starts
         self.processing_end_time = None
         self.progress_results_file_name = output_file_name  # File to persist results
-       
+
         # Tracking
         self.progress_tracker = {}  # Dictionary indexed by folder path for fast lookup
-        #self.current_processing_file = None  # Track the file currently being processed
+        # self.current_processing_file = None  # Track the file currently being processed
 
     def start_processing(self):
         self.processing_start_time = datetime.utcnow()
@@ -69,68 +70,109 @@ class ProcessingResult:
             # Check if directory exists
             if os.path.isdir(full_folder_path):  
                 self.total_paths_to_process.append(full_folder_path)
+                
             else:
                 raise FileNotFoundError(f"Directory does not exist: {full_folder_path}")
+            
         self.save_results()
 
     def set_total_file_types(self, file_type_list: list):
-        # if file_type not in self.file_types_to_process:
-        #     self.file_types_to_process.append(file_type)
-        
         # Copy list into self.file_types_to_process
+        self.total_file_types_to_process = list(set(file_type_list))
         self.save_results()
 
-    def add_folder_to_process(self, folder_path: str, total_media_count: int):
-        if folder_path not in self.progress_tracker:
-            self.progress_tracker[folder_path] = {
-                "folder_name": ""
-                "total_media_count": total_media_count,
-                "detected_media_count": {},
+
+    
+    def add_folder_to_tracking(self, folder_path:str, total_count: int, media_count: dict):
+        if folder_path not in self.progress_tracker: 
+            
+            detected_dict = {}
+            
+            for key in media_count: 
+                detected_dict[key] = {
+                        "total_count": media_count[key],
+                        "processed_count": 0,
+                        "last_processed_path": None,
+                        "processed_source_paths" :  [],
+                        "processed_destination_paths" :  [],
+                        "errors_source_paths" :  [], 
+                    }            
+            
+            self.progress_tracker[os.path.basename(folder_path)] = {
+                "folder_path": folder_path,
+                "total_media_count": total_count,
+                "detected_media_count": detected_dict,
                 "current_processing_file": None
             }
         self.save_results()
+    
 
-    def add_file_progress(self, folder_path: str, file_type: str):
-        if folder_path in self.progress_tracker:
-            if file_type not in self.progress_tracker[folder_path]["detected_media_count"]:
-                self.progress_tracker[folder_path]["detected_media_count"][file_type] = {
-                    "processed_count": 0,
-                    "current_processing_file_path": None,
-                    "processed_source_paths": [],
-                    "processed_destination_paths": [],
-                    "errors_source_paths": []
-                }
-        self.save_results()
+    # def add_folder_to_process(self, folder_path: str):
+    #     if folder_path not in self.progress_tracker:
+    #         self.progress_tracker[folder_path] = {
+    #             "folder_name": "",
+    #             "total_media_count": ,
+    #             "detected_media_count": {},
+    #             "current_processing_file": None
+    #         }
+    #     self.save_results()
 
-    def update_current_processing_file(self, folder_path: str, file_type: str, file_path: str):
-        if folder_path in self.progress_tracker and file_type in self.progress_tracker[folder_path]["detected_media_count"]:
-            self.progress_tracker[folder_path]["detected_media_count"][file_type]["current_processing_file_path"] = file_path
-            self.current_processing_file = file_path
-            self.save_results()
+    # def add_file_progress(self, folder_path: str, file_type: str):
+    #     if folder_path in self.progress_tracker:
+    #         if file_type not in self.progress_tracker[folder_path]["detected_media_count"]:
+    #             self.progress_tracker[folder_path]["detected_media_count"][file_type] = {
+    #                 "processed_count": 0,
+    #                 "current_processing_file_path": None,
+    #                 "processed_source_paths": [],
+    #                 "processed_destination_paths": [],
+    #                 "errors_source_paths": []
+    #             }
+    #     self.save_results()
 
-    def finalize_processed_file(self, folder_path: str, file_type: str, source_path: str, dest_path: str):
-        if folder_path in self.progress_tracker and file_type in self.progress_tracker[folder_path]["detected_media_count"]:
-            file_data = self.progress_tracker[folder_path]["detected_media_count"][file_type]
-            file_data["processed_count"] += 1
-            file_data["processed_source_paths"].append(source_path)
-            file_data["processed_destination_paths"].append(dest_path)
-            file_data["current_processing_file_path"] = None
-            self.current_processing_file = None
-            self.save_results()
+    # def update_current_processing_file(self, folder_path: str, file_type: str, file_path: str):
+    #     if folder_path in self.progress_tracker and file_type in self.progress_tracker[folder_path]["detected_media_count"]:
+    #         self.progress_tracker[folder_path]["detected_media_count"][file_type]["current_processing_file_path"] = file_path
+    #         self.current_processing_file = file_path
+    #         self.save_results()
+
+    # def finalize_processed_file(self, folder_path: str, file_type: str, source_path: str, dest_path: str):
+    #     if folder_path in self.progress_tracker and file_type in self.progress_tracker[folder_path]["detected_media_count"]:
+    #         file_data = self.progress_tracker[folder_path]["detected_media_count"][file_type]
+    #         file_data["processed_count"] += 1
+    #         file_data["processed_source_paths"].append(source_path)
+    #         file_data["processed_destination_paths"].append(dest_path)
+    #         file_data["current_processing_file_path"] = None
+    #         self.current_processing_file = None
+    #         self.save_results()
 
     def save_results(self):
+        # return
         with open(self.progress_results_file_name, "w") as f:
             json.dump(self.summary(), f, indent=4, default=str)
 
     def summary(self):
         return {
-            "paths_to_process": self.paths_to_process,
-            "file_types_to_process": self.file_types_to_process,
+            "total_paths_to_process": self.total_paths_to_process,
+            "total_file_types_to_process": self.total_file_types_to_process,
             "processing_start_time": self.processing_start_time,
             "processing_end_time": self.processing_end_time,
-            "progress_tracker": self.progress_tracker,
-            "current_processing_file": self.current_processing_file
+            # "current_processing_file": self.current_processing_file,
+            "progress_tracker": self.progress_tracker
         }
 
     def print_summary(self):
         print(json.dumps(self.summary(), indent=4, default=str))
+
+    
+    def get_total_paths_to_process(self): 
+        return self.total_paths_to_process
+    def get_total_file_types_to_process(self): 
+        return self.total_file_types_to_process
+    def get_processing_start_time(self): 
+        return self.processing_start_time
+    def get_processing_end_time(self): 
+        return self.processing_end_time
+    def get_progress_results_file_name(self): 
+        return self.progress_results_file_name
+    def get_progress_tracker(self): 
+        return self.progress_tracker
